68400c6095c85f8263cd62438aaf6c298a2ff3e5
Pratap <skondreddy@presidio.com>
2023-01-05 14:59:03 +0530

    Entity Framework NuGet Packages added - Ran the Scaffold DbContext

---
+++ CRUDDotNet6/Models/studentsDBContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CRUDDotNet6.Models
+{
+    public partial class studentsDBContext : DbContext
+    {
+        public studentsDBContext()
+        {
+        }
+
+        public studentsDBContext(DbContextOptions<studentsDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseMySql("server=sp-dot-net.cdgkfoacvf6u.us-east-1.rds.amazonaws.com;user=admin;password=DotNet123;database=studentsDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.28-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8mb4_0900_ai_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.ToTable("students");
+
+                entity.HasIndex(e => e.Email, "email_UNIQUE")
+                    .IsUnique();
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.City)
+                    .HasMaxLength(45)
+                    .HasColumnName("city");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(45)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(60)
+                    .HasColumnName("name");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}

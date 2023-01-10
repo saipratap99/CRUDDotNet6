df38c4b1b464cad443ebd6094433c814afe21a04
Pratap <skondreddy@presidio.com>
2023-01-10 23:03:58 +0530

    Added azure function

---
+++ CalculatorAdditionFunction/CalculatorAdditionFunction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace CalculatorAdditionFunction
+{
+    public static class CalculatorAdditionFunction
+    {
+        [FunctionName("CalculatorAdditionFunction")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            //string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            int a = data?.a;
+            int b = data?.b;
+
+            string responseMessage = $"{a} + {b} = {a+b}";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
+

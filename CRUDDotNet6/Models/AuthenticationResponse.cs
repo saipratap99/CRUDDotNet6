90334a4f48fb1ff48c73ec8e7256d84ac51defd6
Pratap <skondreddy@presidio.com>
2023-01-10 14:17:11 +0530

    Added msg to auth respone

--- CRUDDotNet6/Models/AuthenticationResponse.cs
+++ CRUDDotNet6/Models/AuthenticationResponse.cs
@@ -4,9 +4,11 @@ namespace CRUDDotNet6.Models
 	public class AuthenticationResponse
 	{
 		public string JWTToken { get; set; }
+		public string msg { get; set; }
 		public AuthenticationResponse(string token)
 		{
 			this.JWTToken = token;
+			this.msg = "JWT expries in 1hr";
 		}
 	}
 }
